5a74ac9a20edbf380ff7a3f66cc9573ab7ed1958
rainu <rainu@raysha.de>
2018-01-27 20:56:54 +0100

    return 404 if version file was not found

--- server/web/src/routes/version.js
+++ server/web/src/routes/version.js
@@ -2,15 +2,21 @@
 
 let path = require('path');
 let fs = require('fs');
+const HttpStatus = require('http-status-codes');
 let config = require('../../../common/src/config');
 
 const router = require('express').Router();
 
 router.route('/version')
   .get((req, resp) => {
-    resp.set('Content-Type', 'application/json');
-    fs.createReadStream(path.join(config.server.asset.static, '/version'))
-    .pipe(resp);
+    if(!fs.existsSync(path.join(config.server.asset.static, '/version'))){
+      resp.status(HttpStatus.NOT_FOUND);
+      resp.end();
+    }else{
+      resp.set('Content-Type', 'application/json');
+      fs.createReadStream(path.join(config.server.asset.static, '/version'))
+        .pipe(resp);
+    }
   });
 
 module.exports = router;

e3ef649b76ae0a122886cd718be6656d97833e0d
rainu <rainu@raysha.de>
2018-02-24 00:13:00 +0100

    add message components

--- client/js/main.js
+++ client/js/main.js
@@ -10,6 +10,10 @@ import { routes } from './routes.js';
 import {fnumber, number, euro, date} from './filter'
 import VueGoodTable from 'vue-good-table';
 import Box from '../components/general/Box'
+import Error from '../components/general/message/Error'
+import Info from '../components/general/message/Info'
+import Warning from '../components/general/message/Warning'
+import Success from '../components/general/message/Success'
 import App from '../components/App.vue'
 
 import {GridLayout, GridItem} from 'vue-grid-layout'
@@ -23,6 +27,10 @@ Vue.filter('euro', euro);
 Vue.filter('date', date);
 
 Vue.component('box', Box);
+Vue.component('message-error', Error);
+Vue.component('message-info', Info);
+Vue.component('message-warning', Warning);
+Vue.component('message-success', Success);
 Vue.component('grid-layout', GridLayout);
 Vue.component('grid-item', GridItem);
 
